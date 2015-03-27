ff0f4c1cb022f6aa1f0dc2213acb272a791446d3
Shadowfacts <akorde@icloud.com>
2015-03-27 13:55:17 -0400

    WIP Module Manager

---
+++ src/main/java/baymaxirc/core/module/ModuleManager.java
@@ -0,0 +1,17 @@
+package baymaxirc.core.module;
+
+import java.util.ArrayList;
+
+/**
+ * Searches all classes for @Module annotations, indicated the class is a Baymax Module to be loaded.
+ * @author shadowfacts
+ */
+public class ModuleManager {
+
+	private static ArrayList<Class> modules = new ArrayList<>();
+
+	public static void loadModules() {
+
+	}
+
+}
