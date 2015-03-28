307ee2b1126eaf3645dffea380c5f3fedcb5cf0d
Shadowfacts <akorde@icloud.com>
2015-03-28 10:48:54 -0400

    Oops

---
+++ src/main/java/baymaxirc/core/module/IModule.java
@@ -0,0 +1,29 @@
+package baymaxirc.core.module;
+
+import baymaxirc.core.event.IEventHandler;
+
+/**
+ * @author shadowfacts
+ */
+public interface IModule {
+
+	/**
+	 * @return The name of the module.
+	 */
+	String getName();
+
+	/**
+	 * Register all commands here.
+	 */
+	default void registerCommands() {
+
+	}
+
+	/**
+	 * @return The event handler.
+	 */
+	default IEventHandler getModuleEventHandler() {
+		return null;
+	}
+
+}
