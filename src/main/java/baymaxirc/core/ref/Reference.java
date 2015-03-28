57928203207bdbf69eedfff42b95bc2a15657dac
Shadowfacts <akorde@icloud.com>
2015-03-28 11:31:57 -0400

    Change default nick

--- src/main/java/baymaxirc/core/ref/Reference.java
+++ src/main/java/baymaxirc/core/ref/Reference.java
@@ -7,7 +7,7 @@ public class Reference {
 
 	public static final String version = "1.0.0";
 
-	public static final String name = "Baymax";
+	public static final String name = "BaymaxIRC";
 	public static final String realName = "Your personal IRC companion.";
 
 }
