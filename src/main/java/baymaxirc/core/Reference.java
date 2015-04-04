f2feab259cbe73b0e7406e72f8c3ea02b1948999
shadowfacts <shadowfacts@sif.local>
2015-04-04 10:56:39 -0400

    Remove more old code

--- src/main/java/baymaxirc/core/Reference.java
+++ src/main/java/baymaxirc/core/Reference.java
@@ -7,7 +7,4 @@ public class Reference {
 
 	public static final String version = "1.1.2.1";
 
-	public static final String name = "BaymaxIRC";
-	public static final String realName = "Your personal IRC companion.";
-
 }
